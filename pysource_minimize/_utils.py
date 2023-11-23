2ac22ee8886e101a472fa95eae2cecf63dfe4812
Frank Hoffmann <15r10nk-git@polarbit.de>
2023-11-23 19:51:58 +0100

    refactor: extracted parse function

--- pysource_minimize/_utils.py
+++ pysource_minimize/_utils.py
@@ -6,4 +6,14 @@ if sys.version_info >= (3, 9):
 else:
     from astunparse import unparse  # type: ignore
 
+import ast
+
+
+def parse(source: str) -> ast.Module:
+    if sys.version_info >= (3, 8):
+        return ast.parse(source, type_comments=True)
+    else:
+        return ast.parse(source)
+
+
 __all__ = ("unparse",)
