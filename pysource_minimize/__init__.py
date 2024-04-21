cd9e13eee421564081570258fd8a885451affbaa
Alex Waygood <alex.waygood@gmail.com>
2024-04-21 01:05:45 +0100

    Improve error reporting when source cannot be minimized

--- pysource_minimize/__init__.py
+++ pysource_minimize/__init__.py
@@ -1,8 +1,8 @@
-from ._minimize import minimize
+from ._minimize import minimize, CouldNotMinimize
 from ._minimize_base import StopMinimization
 
 
-__all__ = ("minimize", "StopMinimization")
+__all__ = ("minimize", "CouldNotMinimize", "StopMinimization")
 
 
 version = "0.6.2"
